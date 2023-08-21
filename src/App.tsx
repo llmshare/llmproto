f303f9d787814a6f7ab1ce400c6c24dfa35a1376
Jotiraditya Banerjee <74965372+praetorian2710@users.noreply.github.com>
2023-08-21 12:04:14 +0000

    Draft

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Editor from "./Editor.tsx"
+function App() {
+
+  return (
+    <>
+     <Editor/>
+    </>
+  )
+}
+
+export default App
