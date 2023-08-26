d942fd7f369a9d36368f7fddde1c2628b9306da1
Ashutosh Rai <ashu00jnp@gmail.com>
2023-08-27 03:34:21 +0530

    added textNode

--- reteJS/test/src/editor.ts
+++ reteJS/test/src/editor.ts
@@ -1,16 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { NodeEditor, GetSchemes, ClassicPreset } from "rete";
+import { NodeEditor, GetSchemes, ClassicPreset, NodeId } from "rete";
 import { AreaPlugin, AreaExtensions } from "rete-area-plugin";
 import {
   ConnectionPlugin,
   Presets as ConnectionPresets
 } from "rete-connection-plugin";
 import { ReactPlugin, Presets, ReactArea2D } from "rete-react-plugin";
-import {
-  AutoArrangePlugin,
-  Presets as ArrangePresets
-} from "rete-auto-arrange-plugin";
-import { DataflowEngine, ControlFlowEngine } from "rete-engine";
+import { DataflowEngine } from "rete-engine";
 import {
   ContextMenuExtra,
   ContextMenuPlugin,
@@ -45,7 +41,7 @@ class Log extends ClassicPreset.Node<
   height = 150;
 
   constructor(
-    private logs: (text: string) => void,
+    private log: (text: string) => void,
     private dataflow: DataflowEngine<Schemes>
   ) {
     super("Log");
@@ -60,7 +56,7 @@ class Log extends ClassicPreset.Node<
       message: string[];
     };
 
-    this.logs((inputs.message && inputs.message[0]) || "");
+    this.log((inputs.message && inputs.message[0]) || "");
 
     forward("exec");
   }
@@ -78,13 +74,13 @@ class TextNode extends ClassicPreset.Node<
   height = 120;
   width = 180;
 
-  constructor(initial: string) {
-    super("Text");
+  constructor(initial: string,final:string) {
+    super("Import");
     this.addControl(
       "value",
-      new ClassicPreset.InputControl("text", { initial })
+      new ClassicPreset.InputControl("text", { initial}),
     );
-    this.addOutput("value", new ClassicPreset.Output(socket, "Number"));
+        this.addOutput("value", new ClassicPreset.Output(socket, "Output"));
   }
 
   execute() {}
@@ -95,102 +91,19 @@ class TextNode extends ClassicPreset.Node<
     };
   }
 }
-class NumberNode extends ClassicPreset.Node<
-  {},
-  { value: ClassicPreset.Socket },
-  { value: ClassicPreset.InputControl<"number"> }
-> {
-  height = 120;
-  width = 180;
 
-  constructor(initial: number, change?: () => void) {
-    super("Number");
-    this.addControl(
-      "value",
-      new ClassicPreset.InputControl("number", { initial, change })
-    );
-    this.addOutput("value", new ClassicPreset.Output(socket, "Number"));
-  }
 
-  data(): { value: number } {
-    return {
-      value: this.controls.value.value || 0
-    };
-  }
-}
-
-class AddNode extends ClassicPreset.Node<
-  { left: ClassicPreset.Socket; right: ClassicPreset.Socket },
-  { value: ClassicPreset.Socket },
-  { value: ClassicPreset.InputControl<"number"> }
-> {
-  height = 190;
-  width = 180;
-
-  constructor(
-    change?: () => void,
-    private update?: (control: ClassicPreset.InputControl<"number">) => void
-  ) {
-    super("Adder");
-    const left = new ClassicPreset.Input(socket, "num1");
-    const right = new ClassicPreset.Input(socket, "num2");
-
-    left.addControl(
-      new ClassicPreset.InputControl("number", { initial: 0, change })
-    );
-    right.addControl(
-      new ClassicPreset.InputControl("number", { initial: 0, change })
-    );
-
-    this.addInput("left", left);
-    this.addInput("right", right);
-    this.addControl(
-      "value",
-      new ClassicPreset.InputControl("number", {
-        readonly: true
-      })
-    );
-    this.addOutput("value", new ClassicPreset.Output(socket, "Number"));
-  }
-
-  data(inputs: { left?: number[]; right?: number[] }): { value: number } {
-    const leftControl = this.inputs.left?.control as ClassicPreset.InputControl<
-      "number"
-    >;
-    const rightControl = this.inputs.right
-      ?.control as ClassicPreset.InputControl<"number">;
-
-    const { left, right } = inputs;
-    const value =
-      (left ? left[0] : leftControl.value || 0) +
-      (right ? right[0] : rightControl.value || 0);
-
-    this.controls.value.setValue(value);
-
-    if (this.update) this.update(this.controls.value);
-
-    return { value };
-  }
-}
 
 class Connection<
   A extends Node,
   B extends Node,
 > extends ClassicPreset.Connection<A, B> {}
 
-class ConnectionFlow<
-  A extends NodeProps,
-  B extends NodeProps
-> extends ClassicPreset.Connection<A, B> {
-  isLoop?: boolean;
-}
 
-type Node = NumberNode | AddNode ;
-type NodeProps = Start | Log | TextNode;
-type ConnProps = Connection<NumberNode, AddNode> | Connection<AddNode, AddNode>;
-type ConnectionProps=ConnectionFlow<Start, Log>|ConnectionFlow<TextNode, Log>
+
+type Node = TextNode;
+type ConnProps =  Connection<TextNode,TextNode>;
 type Schemes = GetSchemes<Node, ConnProps>;
-type Schema = GetSchemes<NodeProps, ConnectionProps>;
 
 type AreaExtra = ReactArea2D<any> | ContextMenuExtra;
 
@@ -199,19 +112,7 @@ export async function createEditor(container: HTMLElement) {
   const area = new AreaPlugin<Schemes, AreaExtra>(container);
   const connection = new ConnectionPlugin<Schemes, AreaExtra>();
   const render = new ReactPlugin<Schemes, AreaExtra>({ createRoot });
-  const arrange = new AutoArrangePlugin<Schemes>();
-  const dataflow = new DataflowEngine<Schemes>(({ inputs, outputs }) => {
-    return {
-      inputs: () => Object.keys(inputs).filter((name) => name !== "exec"),
-      outputs: () => Object.keys(outputs).filter((name) => name !== "exec")
-    };
-  });
-  const engines = new ControlFlowEngine<Schema>(() => {
-    return {
-      inputs: () => ["exec"],
-      outputs: () => ["exec"]
-    };
-  });
+  
   const engine = new DataflowEngine<Schemes>();
 
   function process() {
@@ -219,65 +120,28 @@ export async function createEditor(container: HTMLElement) {
 
     editor
       .getNodes()
-      .filter((n) => n instanceof AddNode)
+      .filter((n) => n instanceof TextNode)
       .forEach((n) => engine.fetch(n.id));
   }
-
-
   
-  const contextMenu = new ContextMenuPlugin<Schemes|Schema>({
+  const contextMenu = new ContextMenuPlugin<Schemes>({
     items: ContextMenuPresets.classic.setup([
-      ["Number", () => new NumberNode(0, process)],
-      ["Add", () => new AddNode(process, (c) => area.update("control", c.id))],
-      ["Start", () => new Start()],
-      ["Text", () => new TextNode("")]
+      ["Import",()=> new TextNode("","")]
     ])
   });
   area.use(contextMenu);
-
-  AreaExtensions.selectableNodes(area, AreaExtensions.selector(), {
-    accumulating: AreaExtensions.accumulateOnCtrl()
-  });
 
   render.addPreset(Presets.contextMenu.setup());
   render.addPreset(Presets.classic.setup());
 
   connection.addPreset(ConnectionPresets.classic.setup());
 
-  arrange.addPreset(ArrangePresets.classic.setup());
-
   editor.use(engine);
   editor.use(area);
   area.use(connection);
   area.use(render);
-  area.use(arrange);
 
-  AreaExtensions.simpleNodesOrder(area);
   AreaExtensions.showInputControl(area);
-
-  editor.addPipe((context) => {
-    if (["connectioncreated", "connectionremoved"].includes(context.type)) {
-      process();
-    }
-    return context;
-  });
-
-  const a = new NumberNode(1, process);
-  const b = new NumberNode(1, process);
-  const c = new AddNode(process, (c) => area.update("control", c.id));
-
-  const con1 = new Connection(a, "value", c, "left");
-  const con2 = new Connection(b, "value", c, "right");
-
-  await editor.addNode(a);
-  await editor.addNode(b);
-  await editor.addNode(c);
-
-  await editor.addConnection(con1);
-  await editor.addConnection(con2);
-
-  await arrange.layout();
-  AreaExtensions.zoomAt(area, editor.getNodes());
 
   return {
     destroy: () => area.destroy()
