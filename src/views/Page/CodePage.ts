d320ffb3afda1d59c8293430c5abd121cdfc0945
Suryansh Anand <spirit@anandsur.com>
2023-09-22 14:29:33 +0530

    feat: add context menu to add nodes at will.

--- src/views/Page/CodePage.ts
+++ src/views/Page/CodePage.ts
@@ -2,13 +2,14 @@ import { createRoot } from "react-dom/client";
 import { GetSchemes, NodeEditor } from "rete";
 import { AreaExtensions, AreaPlugin } from "rete-area-plugin";
 import {
-  AutoArrangePlugin,
-  Presets as ArrangePresets,
-} from "rete-auto-arrange-plugin";
-import {
   ConnectionPlugin,
   Presets as ConnectionPresets,
 } from "rete-connection-plugin";
+import {
+  ContextMenuExtra,
+  ContextMenuPlugin,
+  Presets as ContextMenuPresets,
+} from "rete-context-menu-plugin";
 import { Presets, ReactArea2D, ReactPlugin } from "rete-react-plugin";
 
 import { createChain } from "@/controllers/chain";
@@ -25,25 +26,45 @@ import RecursiveCharacterTextSplitterNode from "@/views/Nodes/RecursiveCharacter
 // type Node = OpenAINode | CodeNode;
 type Schemes = GetSchemes<any, any>; // TODO: Need to fix the Schemes type. It needs to hold the right Node type for giving better context in plugin configuration. WORKS FINE FOR NOW.
 
-type AreaExtra = ReactArea2D<any>;
+type AreaExtra = ReactArea2D<any> | ContextMenuExtra;
 
 export default async function createEditor(container: HTMLElement) {
+  const editor = new NodeEditor<Schemes>();
+  const area = new AreaPlugin<Schemes, AreaExtra>(container);
+  const connection = new ConnectionPlugin<Schemes, AreaExtra>();
+  const render = new ReactPlugin<Schemes, AreaExtra>({ createRoot });
+
+  AreaExtensions.selectableNodes(area, AreaExtensions.selector(), {
+    accumulating: AreaExtensions.accumulateOnCtrl(),
+  });
+
   const id = await createLangchain({ name: "summarization" });
 
   const openAI = await createLLMModel(id);
   const chain = await createChain(id);
   const recursiveCharacterTextSplitter = await createTextSplitter(id);
 
-  const editor = new NodeEditor<Schemes>();
-  const area = new AreaPlugin<Schemes, AreaExtra>(container);
-  const connection = new ConnectionPlugin<Schemes, AreaExtra>();
-  const render = new ReactPlugin<Schemes, AreaExtra>({ createRoot });
-  const arrange = new AutoArrangePlugin<Schemes>();
-
-  AreaExtensions.selectableNodes(area, AreaExtensions.selector(), {
-    accumulating: AreaExtensions.accumulateOnCtrl(),
+  const contextMenu = new ContextMenuPlugin<Schemes>({
+    items: ContextMenuPresets.classic.setup([
+      ["LLM", [["OpenAI", () => new OpenAINode(openAI)]]],
+      ["Chain", [["Summarization", () => new ChainNode(chain)]]],
+      [
+        "Text Splitter",
+        [
+          [
+            "Recursive",
+            () =>
+              new RecursiveCharacterTextSplitterNode(
+                recursiveCharacterTextSplitter,
+              ),
+          ],
+        ],
+      ],
+    ]),
   });
+  area.use(contextMenu);
 
+  render.addPreset(Presets.contextMenu.setup());
   render.addPreset(
     Presets.classic.setup({
       customize: {
@@ -68,27 +89,14 @@ export default async function createEditor(container: HTMLElement) {
 
   connection.addPreset(ConnectionPresets.classic.setup());
 
-  arrange.addPreset(ArrangePresets.classic.setup());
-
   editor.use(area);
 
   area.use(connection);
   area.use(render);
-  area.use(arrange);
 
   AreaExtensions.simpleNodesOrder(area);
   AreaExtensions.showInputControl(area);
 
-  const openAINode = new OpenAINode(openAI);
-  const chainNode = new ChainNode(chain);
-  const recursiveCharacterTextSplitterNode =
-    new RecursiveCharacterTextSplitterNode(recursiveCharacterTextSplitter);
-
-  await editor.addNode(openAINode);
-  await editor.addNode(chainNode);
-  await editor.addNode(recursiveCharacterTextSplitterNode);
-
-  await arrange.layout();
   await AreaExtensions.zoomAt(area, editor.getNodes());
 
   return {
